889271e6edd793f39b2b29f767a56b39617a9b89
Erick Lumunge <lumungep12@gmail.com>
2021-05-11 11:18:02 +0300

    about page ans styling smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import About from './Components/About';
 import './App.css';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Header/>
+      <About/>
     </>
   );
 }
