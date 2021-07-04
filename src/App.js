2b539cc313a76724439ba34897cc8d774db5d27f
Erick Lumunge <lumungep12@gmail.com>
2021-07-04 11:39:09 +0300

    rebuild

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import Navbar from './Components/Navbar';
-import Header from './Components/Header';
-import About from './Components/About';
-import Apartments from './Components/Apartments';
-import Works from './Components/Works';
-import Services from './Components/Services';
-import Agents from './Components/Agents';
-import Adverts from './Components/Adverts';
-import Footer from './Components/Footer';
-import './App.css';
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+import About from "./Components/About";
+import Apartments from "./Components/Apartments";
+import Works from "./Components/Works";
+import Services from "./Components/Services";
+import Agents from "./Components/Agents";
+import Adverts from "./Components/Adverts";
+import Footer from "./Components/Footer";
+import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Navbar/>
-      <Header/>
-      <About/>
-      <Apartments/>
-      <Works/>
-      <Services/>
-      <Agents/>
-      <Adverts/>
-      <Footer/>
-    </>
-  );
+	return (
+		<>
+			<h1>Appl Here</h1>
+			<Navbar />
+			<Header />
+			<About />
+			<Apartments />
+			<Works />
+			<Services />
+			<Agents />
+			<Adverts />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
